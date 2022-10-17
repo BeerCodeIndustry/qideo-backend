9bda784b7ba2f6a2bfff1606c884401853b3a171
ChainHokes <k.nazar2003@gmail.com>
2022-10-17 19:53:32 +0300

    init commit

---
+++ kpp/KppProject/src/main/java/com/src/kppproject/StringInversion/StringInvRepository.java
@@ -0,0 +1,2 @@
+package com.src.kppproject.StringInversion;public interface StringInvRepository {
+}
---
+++ kpp/KppProject/src/main/java/com/src/kppproject/StringInversion/dto/StringInverseDto.java
@@ -0,0 +1,2 @@
+package com.src.kppproject.StringInversion.dto;public class StringInverseDto {
+}
